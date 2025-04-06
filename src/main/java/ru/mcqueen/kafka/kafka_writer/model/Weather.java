1dbbdc8ae3ba31979d9d0145ea420318cbc9d50c
anastasia makunina <asetorn@yandex.ru>
2025-04-06 22:44:07 +0300

    Сервис пишет в кафку (аннотации ломбока не заработали)

--- src/main/java/ru/mcqueen/kafka/kafka_writer/model/Weather.java
+++ src/main/java/ru/mcqueen/kafka/kafka_writer/model/Weather.java
@@ -1,10 +1,10 @@
 package ru.mcqueen.kafka.kafka_writer.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
 public class Weather {
     private String region;
     private String temperature;
@@ -14,5 +14,41 @@ public class Weather {
 
     public String getRegion() {
         return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    public String getRainfall() {
+        return rainfall;
+    }
+
+    public void setRainfall(String rainfall) {
+        this.rainfall = rainfall;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
     }
 }
