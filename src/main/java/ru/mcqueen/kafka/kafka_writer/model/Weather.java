8cce06de5f4de297a5d039467ca1ed05577326df
anastasia makunina <asetorn@yandex.ru>
2025-04-07 21:59:12 +0300

    Починила Lombok, все работает.

--- src/main/java/ru/mcqueen/kafka/kafka_writer/model/Weather.java
+++ src/main/java/ru/mcqueen/kafka/kafka_writer/model/Weather.java
@@ -1,10 +1,10 @@
 package ru.mcqueen.kafka.kafka_writer.model;
 
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Weather {
     private String region;
     private String temperature;
@@ -12,43 +12,4 @@ public class Weather {
     private String rainfall;
     private String humidity;
 
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(String temperature) {
-        this.temperature = temperature;
-    }
-
-    public String getWind() {
-        return wind;
-    }
-
-    public void setWind(String wind) {
-        this.wind = wind;
-    }
-
-    public String getRainfall() {
-        return rainfall;
-    }
-
-    public void setRainfall(String rainfall) {
-        this.rainfall = rainfall;
-    }
-
-    public String getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(String humidity) {
-        this.humidity = humidity;
-    }
 }
